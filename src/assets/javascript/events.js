032bda1b7bb0904a9f98191316a613a84d03a3a0
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-06-05 01:35:06 +0300

    fix: update filter mechanism

--- src/assets/javascript/events.js
+++ src/assets/javascript/events.js
@@ -4,5 +4,5 @@ import * as task from './task';
 export default function loadEvents() {
   constants.taskButton.addEventListener('click', task.createTask);
   constants.taskList.addEventListener('click', task.operationWithTask);
-  constants.filterOption.addEventListener('click', task.filterTasks);
+  constants.filterOption.addEventListener('change', task.filterTasks);
 }
